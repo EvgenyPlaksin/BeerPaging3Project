c9633a8eff1b6083a13ea7665b9c81521a22c43d
Lnight <plaksinevgeniy766@gmail.com>
2023-05-16 18:31:38 +0200

    Initial commit

---
+++ app/src/main/java/com/lnight/beerpaging3project/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.lnight.beerpaging3project
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.lnight.beerpaging3project.ui.theme.BeerPaging3ProjectTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        setContent {
+            BeerPaging3ProjectTheme {
+                val systemUiController = rememberSystemUiController()
+                val darkColors = !isSystemInDarkTheme()
+
+                SideEffect {
+                    systemUiController.setSystemBarsColor(
+                        Color.Transparent,
+                        darkColors
+                    )
+                }
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
